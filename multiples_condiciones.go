74a8912583e43e3a65db574f29a513c7d0015407
Jose Martin Rangel Espinoza <jose.rangelespinoza@mercadolibre.com.mx>
2021-03-26 11:56:18 -0600

    multiples condiciones

---
+++ multiples_condiciones.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	var calificacion int
+
+	fmt.Print("Ingresa una califiación:")
+	fmt.Scanf("%d", &calificacion)
+
+	if calificacion == 10 {
+		fmt.Println("Felicidades, obtuviste una calificación perfecta")
+	} else if calificacion == 8 || calificacion == 9 {
+		fmt.Println("Aprobaste la materia.")
+	} else if calificacion == 6 || calificacion == 7 {
+		fmt.Println("Aprobaste la materia, pero necesitas estudiar más.")
+	} else if calificacion >= 0 && calificacion <= 5 {
+		fmt.Println("No aprobaste la materia.")
+	} else {
+		fmt.Println("Calificación no válida.")
+	}
+
+	// if calificacion == 10 {
+	// 	fmt.Println("Felicidades, obtuviste una calificación perfecta")
+	// } else {
+	// 	if calificacion == 8 || calificacion == 9 {
+	// 		fmt.Println("Aprobaste la materia.")
+	// 	} else {
+	// 		if calificacion == 6 || calificacion == 7 {
+	// 			fmt.Println("Aprobaste la materia, pero necesitas estudiar más.")
+	// 		} else {
+	// 			if calificacion >= 0 && calificacion <= 5 {
+	// 				fmt.Println("No aprobaste la materia.")
+	// 			} else {
+	// 				fmt.Println("Calificación no válida.")
+	// 			}
+	// 		}
+	// 	}
+	// }
+}
